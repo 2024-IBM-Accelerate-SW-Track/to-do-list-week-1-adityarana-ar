7905225536baad6da49abf31be78af1115bd83d1
raditya2023F <adityarana2003.ar@gmail.com>
2024-06-06 15:39:44 -0400

    Passed both sets of tests

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,39 @@
 import React, { Component } from 'react';
-  
+import "./About.css";
+import profilePic from "/Users/adityarana/github-classroom/2024-IBM-Accelerate-SW-Track/to-do-list-week-1-adityarana-ar/src/assets/IMG_7988.JPG";
+
 export default class About extends Component {
   render() {
     return (
       <div>
-        <p>Design your About me page </p> 
+        <div>
+          <div className="split left">
+            <div className="centered">
+              <img
+                className="profile_image"
+                // Image goes here
+                src={profilePic}
+                alt="Profile Pic"
+              ></img>
+            </div>
+          </div>
+          <div className="split right">
+            <div className="centered">
+              <div className="name_title">Aditya Rana</div>
+              <div className="brief_description">
+                <p>Hi everyone!</p>
+
+                <p>
+                I am a rising junior at Virginia Tech studying computer science.
+                When I am not working, my peers usually find me outdoors practicing my photography, going to the gym, or playing F1 23.
+                Looking forward to working with all the IBM Accelerate program participants.
+                </p>
+                <p>Cheers!</p>
+              </div>
+            </div>
+          </div>
+        </div>
+
       </div>
     )
   }
